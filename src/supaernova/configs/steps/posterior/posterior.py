c1af7246ee6514d99af5dda28ee44576f4ad9b8a
Patrick Armstrong <patrick.james.1998@gmail.com>
2025-08-24 22:18:42 +1000

    fix posterior masks

--- src/supaernova/configs/steps/posterior/posterior.py
+++ src/supaernova/configs/steps/posterior/posterior.py
@@ -235,7 +235,7 @@ class PosteriorConfig(BackendConfig):
     # - MAP -
     random_initial_positions: bool = False
     tolerance: PositiveFloat = 1e-8
-    x_tolerance: NonNegativeFloat = 1e-2
+    x_tolerance: NonNegativeFloat = 1e-3
     f_relative_tolerance: NonNegativeFloat = 0
     f_absolute_tolerance: NonNegativeFloat = 0
     max_iterations: PositiveInt = 2500
