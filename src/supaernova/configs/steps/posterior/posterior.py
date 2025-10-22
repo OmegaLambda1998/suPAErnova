09844c3d1b02c99b816c12e7fb12137fca8cfd15
Patrick Armstrong <patrick.james.1998@gmail.com>
2025-10-22 13:52:23 +1100

    adjust posterior defaults

--- src/supaernova/configs/steps/posterior/posterior.py
+++ src/supaernova/configs/steps/posterior/posterior.py
@@ -240,8 +240,8 @@ class PosteriorConfig(BackendConfig):
 
     # - MAP -
     random_initial_positions: bool = False
-    tolerance: PositiveFloat = 1e-3
-    x_tolerance: NonNegativeFloat = 1e-3
+    tolerance: PositiveFloat = 1e-4
+    x_tolerance: NonNegativeFloat = 1e-4
     f_relative_tolerance: NonNegativeFloat = 0
     f_absolute_tolerance: NonNegativeFloat = 0
     max_iterations: PositiveInt = 2500
@@ -250,8 +250,8 @@ class PosteriorConfig(BackendConfig):
     # See [Betancourt et al (2014)](https://arxiv.org/abs/1411.6669) for why 0.8 is optimal for the NUTS Sampler
     target_acceptance_rate: PositiveFloat = 0.8
 
-    u_delta_av_min: float | None = -5
-    u_delta_av_max: float | None = 5
+    u_delta_av_min: float | None = -10
+    u_delta_av_max: float | None = 10
     # u_delta_av_min: float | None = None
     # u_delta_av_max: float | None = None
     u_delta_av_start: float = -0.5
@@ -260,8 +260,8 @@ class PosteriorConfig(BackendConfig):
     u_delta_av_std: float = 1
     u_delta_av_prior: bool = True
 
-    u_latents_min: float | None = -5
-    u_latents_max: float | None = 5
+    u_latents_min: float | None = -10
+    u_latents_max: float | None = 10
     # u_latents_min: float | None = None
     # u_latents_max: float | None = None
     u_latents_mean: float = 0
@@ -273,8 +273,8 @@ class PosteriorConfig(BackendConfig):
     delta_av_mean: float = 0
     delta_av_std: float = 1
 
-    delta_m_min: float | None = -5
-    delta_m_max: float | None = 5
+    delta_m_min: float | None = -10
+    delta_m_max: float | None = 10
     # delta_m_min: float | None = None
     # delta_m_max: float | None = None
     delta_m_start: float = -1.5
@@ -283,8 +283,8 @@ class PosteriorConfig(BackendConfig):
     delta_m_std: float = 1
     delta_m_prior: bool = False
 
-    delta_p_min: float | None = -5
-    delta_p_max: float | None = 5
+    delta_p_min: float | None = -10
+    delta_p_max: float | None = 10
     # delta_p_min: float | None = None
     # delta_p_max: float | None = None
     delta_p_start: float = -0.5
@@ -293,8 +293,8 @@ class PosteriorConfig(BackendConfig):
     delta_p_std: float = 1
     delta_p_prior: bool = False
 
-    bias_min: float | None = -5
-    bias_max: float | None = 5
+    bias_min: float | None = -10
+    bias_max: float | None = 10
     # bias_min: float | None = None
     # bias_max: float | None = None
     bias_start: float = -0.5
